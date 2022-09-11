facdb811190368988deef852bac0ab3c37b46afe
ThierryLyon69 <103730310+ThierryLyon69@users.noreply.github.com>
2022-09-11 19:29:55 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+/************************************Fonction descendre fluidement************************************/
+
+function descendre() {
+
+    window.scrollTo({
+        top: 1000,
+        behavior: 'smooth',
+    });   
+}
+
